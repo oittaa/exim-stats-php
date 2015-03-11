fcad3cb9613a3685da494887eecf1f460594105a
oittaa <oittaa@users.noreply.github.com>
2015-03-11 16:32:38 +0100

    Show current day/hour when queried. Optimized inner loops. Added comments.

--- public_html/getData.php
+++ public_html/getData.php
@@ -9,7 +9,7 @@ switch ($q) {
     case "year":
         $startpoint = "-365days";
         $step = "+1day";
-        $num_steps = 365;
+        $num_steps = 365+1;
         $sqltimestr = "%Y-%m-%d 00:00";
         $phptimestr = "Y-m-d 00:00";
         $jtimestr = "Y-m-d";
@@ -17,7 +17,7 @@ switch ($q) {
     case "month":
         $startpoint = "-30days";
         $step = "+1day";
-        $num_steps = 30;
+        $num_steps = 30+1;
         $sqltimestr = "%Y-%m-%d 00:00";
         $phptimestr = "Y-m-d 00:00";
         $jtimestr = "Y-m-d";
@@ -25,7 +25,7 @@ switch ($q) {
     case "week":
         $startpoint = "-7days";
         $step = "+1hour";
-        $num_steps = 24*7;
+        $num_steps = 24*7+1;
         $sqltimestr = "%Y-%m-%d %H:00";
         $phptimestr = "Y-m-d H:00";
         $jtimestr = "Y-m-d H:00";
@@ -33,7 +33,7 @@ switch ($q) {
     case "day":
         $startpoint = "-1day";
         $step = "+1hour";
-        $num_steps = 24;
+        $num_steps = 24+1;
         $sqltimestr = "%Y-%m-%d %H:00";
         $phptimestr = "Y-m-d H:00";
         $jtimestr = "Y-m-d H:00";
@@ -42,36 +42,42 @@ switch ($q) {
     default:
         $startpoint = "-1hour";
         $step = "+1minute";
-        $num_steps = 60;
+        $num_steps = 60; // Current minute isn't needed, because there's no data for it
         $sqltimestr = "%Y-%m-%d %H:%M";
         $phptimestr = "Y-m-d H:i";
         $jtimestr = "Y-m-d H:i";
 }
 
 
-$s = array();
-array_push($s,array("spamtag","Tagged Spam"));
-array_push($s,array("greylistdefer","Greylisted"));
-array_push($s,array("out","Delivered mail"));
-array_push($s,array("in","Received mail"));
-array_push($s,array("rcptrejec","Rejected RCPT"));
-array_push($s,array("spamrejec","Rejected Spam"));
-array_push($s,array("malware","Rejected Malware"));
+$events = array();
+array_push($events,array("spamtag","Tagged Spam"));
+array_push($events,array("greylistdefer","Greylisted"));
+array_push($events,array("out","Delivered mail"));
+array_push($events,array("in","Received mail"));
+array_push($events,array("rcptrejec","Rejected RCPT"));
+array_push($events,array("spamrejec","Rejected Spam"));
+array_push($events,array("malware","Rejected Malware"));
 
 $cols = array();
 $rows = array();
 $col = array();
-$row = array();
+$emptyrow = array();
 
 $col["id"]="";
 $col["label"]="Date";
 $col["pattern"]="";
 $col["type"]="datetime";
+
+// Initialize columns and rows for date
 array_push($cols,$col);
-foreach ($s as $v) {
+array_push($emptyrow, array("v"=>0,"f"=>null));
+
+// Initialize columns and rows for every event type
+foreach ($events as $v) {
     $col["label"]=$v[1];
     $col["type"]="number";
     array_push($cols,$col);
+    array_push($emptyrow, array("v"=>0,"f"=>null));
 }
 
 
@@ -89,19 +95,15 @@ for ($i = 0; $i < $num_steps; $i++) {
   $datestr = "Date(".date("Y,", strtotime($timestamp));
   $datestr .= str_pad($month, 2, '0', STR_PAD_LEFT);
   $datestr .= date(",d,H,i,s", strtotime($timestamp)).")";
+  $row = $emptyrow;
   $row[0]["v"] = $datestr;
   $row[0]["f"] = date($jtimestr, strtotime($timestamp));
-  $j = 1;
-  foreach ($s as $v) {
-    $row[$j]["v"] = 0;
-    $row[$j]["f"] = null;
-    $j++;
-  }
   while ($result && $result["timestamp"] == $timestamp) {
     $j = 1;
-    foreach ($s as $v) {
+    foreach ($events as $v) {
       if ($result["event"] == $v[0]) {
         $row[$j]["v"] = (int)$result["counter"];
+        break;
       }
       $j++;
     }
